5e4efcf728fde06794e324d6d6c176d817ed73b1
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-03-14 16:16:34 +0800

    Added navigation routes

--- src/constants/Route.ts
+++ src/constants/Route.ts
@@ -1,4 +1,5 @@
 export enum Route {
     MAIN_SCREEN = 'MainScreen',
     LOGIN_SCREEN = 'LoginScreen',
+    TASK_SCREEN = 'TaskScreen'
   }
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,14 +5,16 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Route} from '../constants/Route';
 import MainScreen from '../screens/MainScreen';
 import LoginScreen from '../screens/LoginScreen';
+import TaskScreen from '../screens/TaskScreen';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   const mainStack = () => (
     <>
-      <Stack.Screen name={Route.LOGIN_SCREEN} component={LoginScreen} />
-      <Stack.Screen name={Route.MAIN_SCREEN} component={MainScreen} />
+      {/* <Stack.Screen name={Route.LOGIN_SCREEN} component={LoginScreen} />
+      <Stack.Screen name={Route.MAIN_SCREEN} component={MainScreen} /> */}
+      <Stack.Screen name={Route.TASK_SCREEN} component={TaskScreen} />
     </>
   );
 
