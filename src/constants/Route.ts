c7c0cdfc3a714885652d0aeb85cecc19e58c7b86
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-03-17 16:41:18 +0800

    Added task record navigation and route

--- src/constants/Route.ts
+++ src/constants/Route.ts
@@ -1,5 +1,6 @@
 export enum Route {
     MAIN_SCREEN = 'MainScreen',
     LOGIN_SCREEN = 'LoginScreen',
-    TASK_SCREEN = 'TaskScreen'
+    TASK_SCREEN = 'TaskScreen',
+    TASK_RECORD_SCREEN = 'TaskRecordScreen',
   }
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -6,6 +6,7 @@ import {Route} from '../constants/Route';
 import MainScreen from '../screens/MainScreen';
 import LoginScreen from '../screens/LoginScreen';
 import TaskScreen from '../screens/TaskScreen';
+import TaskRecordScreen from '../screens/TaskRecordScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ function RootStack() {
       <Stack.Screen name={Route.LOGIN_SCREEN} component={LoginScreen} />
       <Stack.Screen name={Route.MAIN_SCREEN} component={MainScreen} />
       <Stack.Screen name={Route.TASK_SCREEN} component={TaskScreen} />
+      <Stack.Screen name={Route.TASK_RECORD_SCREEN} component={TaskRecordScreen} />
     </>
   );
 
