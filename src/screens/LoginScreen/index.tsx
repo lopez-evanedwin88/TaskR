83ef4ac81a28a9f439687241f0a802b4d98c2159
Evan Edwin Lopez <lopez.evanedwin@gmail.com>
2024-03-17 13:09:01 +0800

    Added auth login

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,11 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TextInput, Button as TextButton} from 'react-native';
 import styles from './styles';
 import globalStyles from '../../styles/GlobalStyles';
 import {fonts} from '../../styles/Base';
 import Button from '../../component/Button';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../../redux/types';
+import {loginRequest} from '../../redux/login/actions';
+import { Route } from '../../constants/Route';
 
 const LoginScreen = ({navigation}: {navigation: any}) => {
+  const [staff_id, setStaffId] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const {user, loading, error} = useSelector((state: RootState) => state.auth);
+
+  const handleLogin = () => {
+    dispatch(loginRequest(staff_id, password) as any);
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigation.navigate(Route.MAIN_SCREEN);
+    }
+  }, [user]);
+
   return (
     <View
       style={[
@@ -26,6 +45,8 @@ const LoginScreen = ({navigation}: {navigation: any}) => {
           style={styles.txtInputStyle}
           autoCorrect={false}
           autoCapitalize={'none'}
+          value={staff_id}
+          onChangeText={setStaffId}
         />
       </View>
       <View style={[globalStyles.flexDirectionRow, styles.txtInputStyleView]}>
@@ -35,10 +56,12 @@ const LoginScreen = ({navigation}: {navigation: any}) => {
           autoCorrect={false}
           autoCapitalize={'none'}
           secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
         />
       </View>
       <View style={[globalStyles.padding8, globalStyles.flexDirectionRow]}>
-        <Button title="Sign in" onPress={() => {}} style={globalStyles.flex1} />
+        <Button title="Sign in" onPress={handleLogin} style={globalStyles.flex1} />
       </View>
       <View style={[globalStyles.padding8, globalStyles.flexDirectionRow]}>
         <Text style={[{fontSize: fonts.xxlg, alignSelf: 'center'}]}>
